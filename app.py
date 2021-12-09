a26b84380f555d0d7b6878d21fcd8fde694f69d3
Jonah Quist <jonahquist@live.com>
2021-12-08 17:56:57 -0800

    created objects

--- app.py
+++ app.py
@@ -35,13 +35,48 @@ def STEAM_REST(service, request, version, **kwargs):
 class User:
     
     def __init__(self, steamid: str):
-        playerSummary = STEAM_REST("ISteamUser", "GetPlayerSummaries", "v0002", key=api_key, steamids = steamid)
-        friendList = STEAM_REST("ISteamUser", "GetFriendList", "v0002", key=api_key, steamids = steamid)
+        self.playerSummaryJSON = STEAM_REST("ISteamUser", "GetPlayerSummaries", "v0002", key=api_key, steamids = steamid)
         
+        self.steamid=steamid
+        self.mainuser = False
+        
+        self.personaname=self.playerSummaryJSON["response"]["players"][0]["personaname"]
+        self.avatar=self.playerSummaryJSON["response"]["players"][0]["avatar"]
+        
+    def __str__(self):
+        return ""
 
-    def requestPlayerSummary(self, steamid):
-         pass
+class MainUser(User):
+    
+    def __init__(self, steamid: str):
+        super.__init__(self, steamid)
+        
+        self.mainuser = True
+        
+        self.friendListJSON = STEAM_REST("ISteamUser", "GetFriendList", "v0001", key=api_key, steamid = steamid, relationship = "friend")
+        self.ownedGamesJSON = STEAM_REST("IPlayerService", "GetOwnedGames", "v0001", key=api_key, steamid = steamid, include_appinfo = True)
+         
+        self.games = [Game[x] for x in self.ownedGamesJSON["response"]["games"]]
+        self.game_count = self.ownedGamesJSON["response"]["game_count"]
+        
+        self.friends = [User[x] for x in self.friendListJSON["friendslist"]["friends"]]
+
+
+
+class Game:
+    
+    def __init__(self, gameJson):
+        self.appid = gameJson['appid']
+        self.img_logo_url = gameJson['img_logo_url']
+        self.name = gameJson['name']
+        self.playtime_forever = gameJson['playtime_forever']
+        if "playtime_2weeks" in gameJson:
+            self.playtime_2weeks = gameJson['playtime_2weeks']
+        else:
+            self.playtime_2weeks = 0
+           
+        
+        
      
-jsonfile = STEAM_REST("ISteamUser", "GetPlayerSummaries", "v0002", key = api_key, steamids = "76561197960435530")
+jsonfile = STEAM_REST("ISteamUser", "GetFriendList", "v0001", key=api_key, steamid = "76561197960435530", relationship = "friend")
 print(pretty(jsonfile))
-
