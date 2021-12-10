a3150b90eb97e12784b1f0791702687337aa30d3
Jonah Quist <jonahquist@live.com>
2021-12-09 22:36:07 -0800

    added more analysis

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def home():
     return render_template(
         'home.html'
     )
-    
-    
+
 def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 
@@ -43,8 +42,6 @@ def RAWG_REST(*args, **kwargs):
     url = url + "?" + urllib.parse.urlencode(dataDict) # https://api.
     return json.loads(safeget(url).read())
 
-
-
 class User:
     
     def __init__(self, steamid: str):
@@ -59,6 +56,16 @@ class User:
     def __str__(self):
         return ""
 
+class FriendUser(User):
+    
+    def __init__(self, steamid: str, friendsince):
+        User.__init__(self, steamid = steamid)
+        
+        self.friendsince = friendsince
+
+    def __repr__(self):
+        return f"{self.personaname} friends since {self.friendsince}"
+    
 class MainUser(User):
     
     def __init__(self, steamid: str):
@@ -70,23 +77,46 @@ class MainUser(User):
         self.ownedGamesJSON = STEAM_REST("IPlayerService", "GetOwnedGames", "v0001", steamid = steamid, include_appinfo = True)
         
         self.games = []
-        for game in self.ownedGamesJSON["response"]["games"]:
-            self.games.append(Game(game))
+        #for game in self.ownedGamesJSON["response"]["games"]:
+            #self.games.append(Game(game))
+            
+        for i in range(20):
+            self.games.append(Game(self.ownedGamesJSON["response"]["games"][i]))
+            
         #self.games = [Game[x] for x in self.ownedGamesJSON["response"]["games"]]
         self.game_count = self.ownedGamesJSON["response"]["game_count"]
         
         self.friends = []
         for user in self.friendListJSON["friendslist"]["friends"]:
-            self.friends.append(User(user))
+            self.friends.append(FriendUser(user["steamid"], user["friend_since"]))
         #self.friends = [User[x] for x in self.friendListJSON["friendslist"]["friends"]]
+        
+        self.rawgGames = []
 
     def getMostPlayed(self):
         mostPlayed = []
         mostPlayed = sorted(self.games, key=Game.getPlaytime, reverse=True)
+        for i in range(10):
+            mostPlayed[i].getRawg()
+            self.rawgGames.append(mostPlayed[i])
         return mostPlayed
 
+    def getMostGenres(self):
+        genres = {}
+        for game in self.games:
+            if (game not in self.rawgGames):
+                game.getRawg()
+                self.rawgGames.append(game)
+            for genre in game.getGenres():
+                genres[genre] = genres.get(genre, 0) + 1
+        return genres
+            
     def getNewFriends(self):
-        pass
+        newFriends = []
+        for friend in self.friends:
+            if (friend.friendsince > 1609488000):
+                newFriends.append(friend)
+        return newFriends
 
 class Game:
     
@@ -99,17 +129,30 @@ class Game:
             self.playtime_2weeks = gameJson['playtime_2weeks']
         else:
             self.playtime_2weeks = 0
-
+            
     def getPlaytime(self):
         return self.playtime_forever
     
     def __repr__(self):
         return f"{self.name}"
     
+    def getGenres(self):
+        if (self.checkRawg()):
+            genres = []
+            for genre in self.tentativerawg["genres"]:
+                genres.append(genre['name'])
+            return genres
+        else:
+            return []
+    
     def getRawg(self):
-        self.tentativerawg = RAWG_REST("games", search=self.name)["results"][0]["name"]
-        self.tentativerawg["confirmed"] = False
-        
+        self.tentativerawg = RAWG_REST("games", search=self.name)["results"][0]
+        self.tentativerawg["confirmed"] = self.compareRawg()
+        print("checking rawg...")
+    
+    def compareRawg(self):
+        return (self.tentativerawg["name"] == self.name)
+    
     def checkRawg(self):
         if (self.tentativerawg == None or self.tentativerawg["confirmed"] == False):
             return False
@@ -119,9 +162,11 @@ class Game:
 
 
 gamer = MainUser("76561197960434622")
-mostPlayed = gamer.getMostPlayed()
-for i in range(5):
-    print(f"{mostPlayed[i].name} {mostPlayed[i].playtime_forever}")
+#mostPlayed = gamer.getMostPlayed()
+#for i in range(5):
+    #print(f"{mostPlayed[i].name} {mostPlayed[i].playtime_forever}")
+
+print(gamer.getNewFriends())
 """gamer = MainUser("76561197960434622")
 
 print("----Steam----")
